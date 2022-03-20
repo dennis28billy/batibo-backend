1b546256c736f170ad18f3f608111acf863a4059
marshalwuisan <marshalwuisan@gmail.com>
2022-03-20 13:18:36 +0800

    fix route use

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
